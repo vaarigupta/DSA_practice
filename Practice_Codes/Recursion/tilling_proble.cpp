9d760a583a9e4af7254d4a3e0b175e63c74ebb5e
vaari gupta <vaarigupta24@gmail.com>
2019-07-22 04:40:11 +0530

    sorting searching

--- Practice_Codes/Recursion/tilling_proble.cpp
+++ Practice_Codes/Recursion/tilling_proble.cpp
@@ -1,7 +1,29 @@
 #include<iostream>
+#define ll long long
 using namespace std;
+ll tillingWays(ll n ,  ll m)
+{
+    if(n==1 || n==0)
+    {
+        return n;
+    }
+    if((n-m)<=0)
+    {
+        return 1;
+    }
+     ll x = tillingWays(n-1,m);
+     ll y = tillingWays(n-m,m);
+     return 1+x+y;
+}
 int main()
 {
+    ll t;
+    ll n, m;
+    while(t--)
+    {
+        cin>>n>>m;
+        cout<<tillingWays(n,m)<<endl;
+    }
 
     return 0;
 }
