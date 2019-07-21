d7f0276eee626ddba8323d52df28ddc5033052a4
vaari gupta <vaarigupta24@gmail.com>
2019-07-21 16:12:10 +0530

    sort game

--- Practice_Codes/Sorting_Searching/Sort_Game.cpp
+++ Practice_Codes/Sorting_Searching/Sort_Game.cpp
@@ -1,1 +1,45 @@
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+using namespace std;
+bool mycompare(pair<string,int> p1, pair<string,int> p2)
+{
+
+    if(p1.second == p2.second)
+    {
+        return p1.first<p2.first;
+    }
+    return p1.second>p2.second;
+
+}
+
+pair<string,int> p[100005];
+long int n;
+int main()
+{
+
+    int x,k=0,salary;
+    string name;
+    cin>>x;
+    cin>>n;
+
+    for(int i=0;i<n;i++)
+    {
+        cin>>name>>salary;
+        if(salary>=x)
+        {
+            //strcpy(p[k].first,name);
+            p[k].first = name;
+            p[k].second = salary;
+            k++;
+        }
+    }
+    sort(p,p+k,mycompare);
+    for(int i=0;i<k;i++)
+    {
+        cout<<p[i].first<<" "<<p[i].second<<endl;
+    }
+    return 0;
+}
+
 
