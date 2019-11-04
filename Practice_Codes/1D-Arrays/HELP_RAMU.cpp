73f67a73f1e1bbee6072d1165511a88c17823f4a
vaari gupta <vaarigupta24@gmail.com>
2019-11-05 05:17:47 +0530

    arrays

--- Practice_Codes/1D-Arrays/HELP_RAMU.cpp
+++ Practice_Codes/1D-Arrays/HELP_RAMU.cpp
@@ -19,26 +19,36 @@ void help_ramu(int c1, int c2, int c3, int c4, int n, int m, vector<int> rick, v
 	cout<<ans<<endl;
 
 }
+void printVector(vector<int> v)
+{
+    for(int i=0;i<v.size();i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
 int main() {
 	int t,c1,c2,c3,c4,n,m;
 	cin>>t;
 	while(t--)
 	{
     cin>>c1>>c2>>c3>>c4>>n>>m;
-	vector<int> rick(n,0);
-	vector<int> cab(m,0);
+	vector<int> rick;
+	vector<int> cab;
 	for(int i=0;i<n;i++)
 	{
 		int no;
 		cin>>no;
 		rick.push_back(no);
 	}
+	printVector(rick);
 	for(int i=0;i<m;i++)
 	{
 		int no;
 		cin>>no;
 		cab.push_back(no);
 	}
+	printVector(cab);
 	int rickCost =0, cabCost = 0;
 	for(int i=0;i<n;i++)
 	{
@@ -56,3 +66,23 @@ int main() {
 	}
 	return 0;
 }
+
+/*
+
+2
+1 3 7 19
+2 3
+2 5
+4 4 4
+4 3 2 1
+1 3
+798
+1 2 3
+
+
+1
+1 3 7 19
+2 3
+2 5
+4 4 4
+*/
